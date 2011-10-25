970a4e626163cda6a24ab5f61a7a4c03273fe8cf
Erik Rivera <erik.river@gmail.com>
2011-10-25 13:56:47 -0500

    agregando configuracion para produccion

--- templates/wikiconfig.py
+++ templates/wikiconfig.py
@@ -26,146 +26,85 @@
 """
 
 import os
+import glob
 
 from MoinMoin.config import multiconfig, url_prefix_static
+import MoinMoin
+from MoinMoin.config.multiconfig import DefaultConfig
+from MoinMoin.auth import BaseAuth
+from MoinMoin.security import Permissions
+from MoinMoin.user import User
+
+class PlonePermissions(Permissions):
+    """special permission. allow all"""
+
+    def __getattr__(self, attr):
+        return lambda pagename: True
+
+class PloneAuth(BaseAuth):
+    login_inputs = ['username']
+    logout_possible = False
+    name = 'repoze.who'
+
+    def request(self, request, user_obj, **kw):
+        user = request.env.get('repoze.who.identity', {}).get('user', None)
+        if user:
+            user = User(
+                    request, auth_username=user.uid,
+                    auth_method=self.name,
+                    auth_attribs=('name', 'aliasname', 'email', 'password'))
+            user.may = PlonePermissions(user)
+            user.language = 'es'
+            user.valid = 1
+            return user, True
+        return user_obj, True
 
 
 class Config(multiconfig.DefaultConfig):
 
-    # Critical setup  ---------------------------------------------------
-
-    # Directory containing THIS wikiconfig:
     wikiconfig_dir = os.path.abspath(os.path.dirname(__file__))
-
-    # We assume that this config file is located in the instance directory, like:
-    # instance_dir/
-    #              wikiconfig.py
-    #              data/
-    #              underlay/
-    # If that's not true, feel free to just set instance_dir to the real path
-    # where data/ and underlay/ is located:
-    #instance_dir = '/where/ever/your/instance/is'
-    instance_dir = wikiconfig_dir
-
-    # Where your own wiki pages are (make regular backups of this directory):
+    instance_dir = '/Users/erik/Develop/pythonmx_site/var/wiki'
     data_dir = os.path.join(instance_dir, 'data', '') # path with trailing /
-
-    # Where system and help pages are (you may exclude this from backup):
     data_underlay_dir = os.path.join(instance_dir, 'underlay', '') # path with trailing /
-
-    # The URL prefix we use to access the static stuff (img, css, js).
-    # Note: moin runs a static file server at url_prefix_static path (relative
-    # to the script url).
-    # If you run your wiki script at the root of your site (/), just do NOT
-    # use this setting and it will automatically work.
-    # If you run your wiki script at /mywiki, you need to use this:
     #url_prefix_static = '/mywiki' + url_prefix_static
 
-
-    # Wiki identity ----------------------------------------------------
-
-    # Site name, used by default for wiki name-logo [Unicode]
-    sitename = u'Untitled Wiki'
-
-    # Wiki logo. You can use an image, text or both. [Unicode]
-    # For no logo or text, use '' - the default is to show the sitename.
-    # See also url_prefix setting below!
+    sitename = u'PythonMexico'
     logo_string = u'<img src="%s/common/moinmoin.png" alt="MoinMoin Logo">' % url_prefix_static
-
-    # name of entry page / front page [Unicode], choose one of those:
-
-    # a) if most wiki content is in a single language
-    #page_front_page = u"MyStartingPage"
-
-    # b) if wiki content is maintained in many languages
-    #page_front_page = u"FrontPage"
-
-    # The interwiki name used in interwiki links
-    #interwikiname = u'UntitledWiki'
-    # Show the interwiki name (and link it to page_front_page) in the Theme,
-    # nice for farm setups or when your logo does not show the wiki's name.
-    #show_interwiki = 1
-
-
-    # Security ----------------------------------------------------------
-
-    # This is checked by some rather critical and potentially harmful actions,
-    # like despam or PackageInstaller action:
-    #superuser = [u"YourName", ]
-
-    # IMPORTANT: grant yourself admin rights! replace YourName with
-    # your user name. See HelpOnAccessControlLists for more help.
-    # All acl_rights_xxx options must use unicode [Unicode]
-    #acl_rights_before = u"YourName:read,write,delete,revert,admin"
-
-    # The default (ENABLED) password_checker will keep users from choosing too
-    # short or too easy passwords. If you don't like this and your site has
-    # rather low security requirements, feel free to DISABLE the checker by:
-    #password_checker = None # None means "don't do any password strength checks"
-
-    # Link spam protection for public wikis (Uncomment to enable)
-    # Needs a reliable internet connection.
-    #from MoinMoin.security.antispam import SecurityPolicy
-
-
-    # Mail --------------------------------------------------------------
-
-    # Configure to enable subscribing to pages (disabled by default)
-    # or sending forgotten passwords.
-
-    # SMTP server, e.g. "mail.provider.com" (None to disable mail)
-    #mail_smarthost = ""
-
-    # The return address, e.g u"Jürgen Wiki <noreply@mywiki.org>" [Unicode]
-    #mail_from = u""
-
-    # "user pwd" if you need to use SMTP AUTH
-    #mail_login = ""
-
-
-    # User interface ----------------------------------------------------
-
-    # Add your wikis important pages at the end. It is not recommended to
-    # remove the default links.  Leave room for user links - don't use
-    # more than 6 short items.
-    # You MUST use Unicode strings here, but you need not use localized
-    # page names for system and help pages, those will be used automatically
-    # according to the user selected language. [Unicode]
+    page_front_page = u'FrontPage'
+    
     navi_bar = [
-        # If you want to show your page_front_page here:
         #u'%(page_front_page)s',
         u'RecentChanges',
         u'FindPage',
         u'HelpContents',
     ]
 
-    # The default theme anonymous or new users get
     theme_default = 'modern'
+    language_default = 'es'
 
-
-    # Language options --------------------------------------------------
-
-    # See http://moinmo.in/ConfigMarket for configuration in
-    # YOUR language that other people contributed.
-
-    # The main wiki language, set the direction of the wiki pages
-    language_default = 'en'
-
-    # the following regexes should match the complete name when used in free text
-    # the group 'all' shall match all, while the group 'key' shall match the key only
-    # e.g. CategoryFoo -> group 'all' ==  CategoryFoo, group 'key' == Foo
-    # moin's code will add ^ / $ at beginning / end when needed
-    # You must use Unicode strings here [Unicode]
     page_category_regex = ur'(?P<all>Category(?P<key>(?!Template)\S+))'
     page_dict_regex = ur'(?P<all>(?P<key>\S+)Dict)'
     page_group_regex = ur'(?P<all>(?P<key>\S+)Group)'
     page_template_regex = ur'(?P<all>(?P<key>\S+)Template)'
 
-    # Content options ---------------------------------------------------
-
-    # Show users hostnames in RecentChanges
     show_hosts = 1
+    
+    superuser = []
 
     # Enable graphical charts, requires gdchart.
     #chart_options = {'width': 600, 'height': 300}
 
+    auth = [PloneAuth()]
+    cookie_lifetime = 1 # 1 hour after last access ldap login is required again
+    user_autocreate = True
+
+    # we don't allow the user to change those values on UserPreferences page
+    user_form_disable = ['name', 'aliasname', 'email', ]
+    # we remove those fields as they are not used for ldap based logins
+    user_form_remove = ['password', 'password2', ]
+
+    acl_rights_default = u'Known:read,write,admin,delete,revert All:read'
+
+    # liste du cd
+    acl_rights_before  = ','.join(superuser)+':read,write,admin,delete,revert'
+    
